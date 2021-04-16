cd1a5ce7b803e028cea7accad74890b55b15b4fa
Manomite <manomitehq@gmail.com>
2021-04-16 15:34:28 +0100

    Corrected some bugs

--- vendor/mitmelon/guardtor/src/Bayes/TokenizerInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Watermelon\Engine\Bayes;
-
-interface TokenizerInterface
-{
-    public function tokenize($string);
-}
--- vendor/mitmelon/guardtor/src/Limiter/SilentRateLimiter.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace GuardTor\Limiter;
-interface SilentRateLimiter
-{
-    public function limitSilently(string $identifier, Rate $rate): Status;
-}
